7b80273c06359aab837fdb54b90d0ffbbce5f81b
NancyMayek <mayekqua@gmail.com>
2025-05-26 11:23:33 -0600

    Ejercicio 1 terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,42 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+//Codigo original y los cambios que hize antes de hacerlo en una sola funcion  
+// const baseEndpoint = 'https://api.github.com';
+// const usersEndpoint = `${baseEndpoint}/users`;
+// const $n = document.querySelector('.name');//falta el . de selector de clase
+// const $b = document.querySelector('.blog'); //era un selector de clase no de ID
+// const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+// async function displayUser(username) { // async function 
+//   $n.textContent = 'cargando...';
+//   const response = await fetch(`${usersEndpoint}/${username}`); 
+//   const data = await response.json(); //pase la informacion de response de el API a un JSON para que funcionara de manera correcta 
+//   console.log(data);
+//   $n.textContent = `${data.name}`; //cambie las comillas simples por backticks ``
+//   $b.textContent = `${data.blog}`;
+//   $l.textContent = `${data.location}`;
+// }
+
+// function handleError(err) {
+//   console.log('OH NO!');
+//   console.log(err);
+//   $n.textContent = `Algo salió mal: ${err}` //el nombre de la varible $n estaba mal escrito
+// }
+
+// displayUser('stolinski').catch(handleError);
+
+//En esta nueva funcion implemente el try y catch para que est
+const displayUserNew = async (username) => {
+  try{
+  $n = document.querySelector('.name');
+  const response = await fetch(`https://api.github.com/users/${username}`); 
+  const data = await response.json(); //pase la informacion de response de el API a un JSON para que funcionara de manera correcta 
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; //cambie las comillas simples por backticks ``
+  document.querySelector('.blog').textContent = `${data.blog}`;
+  document.querySelector('.location').textContent = `${data.location}`;
+
+  } catch(err){
+    $n.textContent = `Algo salió mal: ${err}`
+  }
 }
 
-function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
-
-displayUser('stolinski').catch(handleError);
+displayUserNew('stolinski');
